028a41fb20b4c60615cd2ad5692bfae96551aa9d
jasho226 <johnjasho0001@gmail.com>
2025-08-07 20:35:54 +0530

    Create Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,15 @@
+package lib;
+
+import java.util.Scanner;
+
+public class Problem2 {
+		public static void main(String []args) {
+			Scanner sc=new Scanner(System.in);
+			System.out.print("Enter the number:");
+			int a=sc.nextInt();
+			for(int i=1;i<a;i++) {
+				System.out.print((i*2)-1 +",");
+			}
+			System.out.print((a*2)-1);
+		}
+}
